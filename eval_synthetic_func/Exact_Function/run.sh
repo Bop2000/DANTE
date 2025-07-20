03c4ec53aa161e2789b2c8ec4cffa2b175fe32a3
Bo Peng <93246595+Bop2000@users.noreply.github.com>
2025-07-20 22:33:21 +0800

    Delete eval_synthetic_func/Exact_Function/run.sh

--- eval_synthetic_func/Exact_Function/run.sh
+++
@@ -1,9 +0,0 @@
-#--func can be: ackley rastrigin rosenbrock schwefel michalewicz griewank levy
-#--method can be: DOTS  DOTS-Greedy DOTS-eGreedy Random DualAnnealing DifferentialEvolution CMA-ES
-
-source $HOME/.bashrc
-
-conda activate tf_25 #your environment
-
-python3 run.py --func rosenbrock --dims 100 --iterations 100000 --method DOTS
-
